de6c28f1d4a1568ed8d4ee2dccff5a7d29f63779
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-10-15 13:59:27 +0200

    Update ExampleMod.java

--- src/main/studio/lunathefox20/ExampleMod.java
+++ src/main/studio/lunathefox20/ExampleMod.java
@@ -3,7 +3,6 @@ package studio.lunathefox20;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod {
@@ -11,7 +10,7 @@ public class ExampleMod {
     public static final String VERSION = "1.0";
 
     @Mod.EventHandler
-    public void serverStarting(FMLServerStartingEvent event) {
-        event.registerServerCommand(new com.example.distancecalculatormod.commands.CommandDistance());
+    public void init(FMLInitializationEvent event) {
+        ClientCommandHandler.instance.registerCommand(new com.example.distancecalculatormod.commands.CommandDistance());
     }
 }
